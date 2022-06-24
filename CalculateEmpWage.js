8620e121a04971c87eaf9061aba0dd13723aa179
mohan3388 <sahumohan338@gmail.com>
2022-06-23 19:31:26 -0700

    [mohan]add: check employee present or not using js

---
+++ CalculateEmpWage.js
@@ -0,0 +1,14 @@
+//uc1 if else condition
+{
+const Is_Present=1
+let empCheck = Math.floor(Math.random()*10)%2;
+
+if(empCheck==Is_Present)
+{
+    console.log("employee is  Present");
+
+}else{
+    console.log("employee is Absent");
+}
+}
+
