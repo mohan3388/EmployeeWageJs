6dd1a3ae183fc3233da789cf47179cf0f3327110
mohan3388 <sahumohan338@gmail.com>
2022-06-23 19:56:40 -0700

    [mohan]add: Calculate EmpWage In Day

--- CalculateEmpWage.js
+++ CalculateEmpWage.js
@@ -12,3 +12,24 @@ if(empCheck==Is_Present)
 }
 }
 
+//Uc2 switch case
+const Is_Fulltime=2;
+const Is_Parttime=1;
+const Parttime_Hours=4;
+const Fulltime_Hours=8;
+const Wage_Per_Hours=20;
+{
+    let empHrs = 0;
+    let empCheck = Math.floor(Math.random()*10)%2;
+    switch(empCheck){
+        case Is_Parttime:
+            empHrs=Parttime_Hours;
+            break;
+        case Is_Fulltime:
+           empHrs=Fulltime_Hours;
+        default:
+            empHrs=0;
+    }
+    let empWage = empHrs*Wage_Per_Hours;
+    console.log("calculate empwage: "+empWage);
+}
