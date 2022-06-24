248af8ec4ef150d60c4398a7bb17bb4432c97e08
mohan3388 <sahumohan338@gmail.com>
2022-06-24 09:47:52 -0700

    [mohan]add: Calculate EmpWage using Object Creation

--- CalculateEmpWage.js
+++ CalculateEmpWage.js
@@ -225,3 +225,27 @@ empDailyHrsMap.forEach((value, key, map) =>{
 console.log("full working days: "+fullWorkingDays);
 console.log("Part working days: "+partWorkingDays);
 console.log("Non working days: "+nonWorkingDays);
+
+//UC10 Object Creation
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+let empDailyHrsAndWageArr = new Array();
+while(totalEmpHrs<= Max_Hrs_In_Month && totalWorkingDays< Number_Of_WorkingDay)
+
+{
+totalWorkingDays++;
+let empCheck = Math.floor(Math.random()*10)%3;
+let empHrs = getWorkingHours(empCheck);
+totalEmpHrs+=empHrs;
+empDailyHrsAndWageArr.push(
+    {
+        dayNum:totalWorkingDays,
+        dailyHours:empHrs,
+        dailyWage:calculateDailWage(empHrs),
+        toString(){
+            return '\nDay'+this.dayNum+' working hour is '+this.dailyHours+' And Wage earned '+this.dailyWage
+        },
+    }
+);
+}
+console.log("showing daily hours worked and wage earned: "+empDailyHrsAndWageArr);
