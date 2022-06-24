19c7b4ae80e2138efbfcbfe3ad60c2035152862a
mohan3388 <sahumohan338@gmail.com>
2022-06-24 02:49:55 -0700

    [mohan]add: Calculate EmpWage In Day

--- CalculateEmpWage.js
+++ CalculateEmpWage.js
@@ -54,3 +54,15 @@ const Wage_Per_Hours=20;
         let empWage = empHrs*Wage_Per_Hours;
         console.log("Total Hrs: "+empHrs+"calculate empwage: "+empWage);
     }
+
+    //Uc4 using for loop calculate empwage
+let TotalEmpHrs=0;
+var Number_Of_WorkingDay=20;
+for(let day=0; day<Number_Of_WorkingDay; day++)
+{
+    let empCheck=Math.floor(Math.random()*10)%3;
+    TotalEmpHrs+=getWorkingHours(empCheck);
+}
+let empWage = TotalEmpHrs*Wage_Per_Hours;
+console.log("Hour: "+TotalEmpHrs+" EmpWage: "+empWage);
+
